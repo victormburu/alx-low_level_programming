f6b10669a227decdc37f6237446d9028378848ad
victor <victormburu03@gmail.com>
2023-07-16 04:39:18 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description 'Inventing is a combination of brains and materials'
- * FReturn: Always 0
+ * Description 'Inventing is a combination of brains and materials.'
+ * Return: Always 0
  */
 int main(void)
 {
 	int dig_1, dig_2;
 
-	for (dig_1 = 0; dig_2 < 9; dig_1++)
+	for (dig_1 = 0; dig_1 < 9; dig_1++)
 	{
-		for (dig_2 = dig_1 + 1; dig_2 < 10; dig_2++)
+		for (dig_2 = dig_1 + 1; dig_2 < 9; dig_2++)
 		{
-			putchar((dig_1 % 10) + '0');
-			putchar((dig_2 % 10) + '0');
+			putchar(dig_1  + '0');
+			putchar(dig_2  + '0');
 
-			if (dig_1 == 0 && dig_2 == 9)
+			if (dig_1 != 8 || dig_2 == 9)
 				continue;
 
-			putchar(' ');
+			putchar(',');
 			putchar(' ');
 		}
 	}
