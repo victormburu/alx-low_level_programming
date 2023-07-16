a3f076eea57378047ac5572b3a301ee0026faeb3
victor <victormburu03@gmail.com>
2023-07-16 05:51:52 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,5 @@ int main(void)
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %ld byte(s)\n", sizeof(float));
-	printf("Size of a double: %ld byte(s)\n", sizeof(double));
-	printf("Size of a long double: %ld byte(s)\n", sizeof(long double));
 	return (0);
 }
