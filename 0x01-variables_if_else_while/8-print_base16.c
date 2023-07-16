ffc665d4c79279fc088e002105ca24bd02010a76
victor <victormburu03@gmail.com>
2023-07-16 04:14:24 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,17 +6,17 @@
  */
 int main(void)
 {
-	 int n;
-	 int m;
+	int n;
+	int m;
 
-	 for (n = 48; n <= 57; n++)
-	 {
-		 putchar(n);
-	 }
-	 for (m = 97; m <= 102; m++)
-	 {
-		 putchar(m);
-	 }
-	 putchar('\n');
-	 return (0);
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m <= 102; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
 }
