abdbdbb33f3b829d67c6256315a5faaf62c3238b
victor <victormburu03@gmail.com>
2023-07-16 06:09:13 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,9 @@
 #include <unistd.h>
-
+/**
+ * main - Entry point
+ * Decscription 'UNIX is basically a simple operating system'
+ * Return: Always 1
+ */
 int main(void)
 {	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
