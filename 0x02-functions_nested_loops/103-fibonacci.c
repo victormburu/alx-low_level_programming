e4f82243041cc1388140b367b0978e765947c7f6
victor <victormburu03@gmail.com>
2023-07-18 11:11:22 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description 'prints the first 98 Fibonacci numbers, starting with 1 and 2'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2, sum = k;
+
+	while (k + j < 4000000)
+	{
+		k += j;
+		if (k % 2 == 0)
+			sum += k;
+		j = k - j;
+		++i;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
