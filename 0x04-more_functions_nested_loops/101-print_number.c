b323197195b8dd681218bce782d2193d0412fe4e
victor <victormburu03@gmail.com>
2023-07-21 02:30:19 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,14 +11,12 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		n *= -1;
-		{
-			k = n;
-			_putchar('_');
-		}
-		k /= 10;
-		if (k != 0)
-			print_number(k);
-		_putchar((unsigned int) n % 10 + '0');
+		_putchar(45);
+		k = -k;
 	}
+	if (k / 10)
+	{
+		print_number(k / 10);
+	}
+	_putchar(k % 10 + '0');
 }
