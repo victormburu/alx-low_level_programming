8055456c6a3e679cd258ff7ef8c4743749758900
victor <victormburu03@gmail.com>
2023-07-27 03:55:03 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,10 +11,7 @@ char *_strncpy(char *dest, char *src, int n)
 	int index = 0, src_len = 0;
 
 	while (src[index++])
-	{
 		src_len++;
-		index++;
-	}
 	for (index = 0; src[index] && index < n; index++)
 		dest[index] = src[index];
 	for (index = src_len; index < n; index++)
