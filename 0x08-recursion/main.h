8a8019c28d6908bf0c288fb647c5c2ce0629a28a
victor <victormburu03@gmail.com>
2023-08-02 15:37:58 -0700

    100-is_palindrome.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,3 +10,5 @@ int wildcmp(char *s1, char *s2);
 int _putchar(char c);
 int actual_sqrt_recursion(int n, int a);
 int actual_prime(int n, int i);
+int check_pal(char *s, int i, int len);
+int _strlen_recurion(char *s);
