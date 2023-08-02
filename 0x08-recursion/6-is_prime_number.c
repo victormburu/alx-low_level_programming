66d38530379dc8b62232d9c2ac7a31a1ac7d17f1
victor <victormburu03@gmail.com>
2023-08-02 15:09:16 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: the number to check
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n - 1));
+}
+/**
+ * actual_prime - cal the prime reculsively
+ * @n: input value
+ * @i: iterator
+ * Return: 1 if is a prime, 0 if not
+ */
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, i - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,3 +9,4 @@ int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int _putchar(char c);
 int actual_sqrt_recursion(int n, int a);
+int actual_prime(int n, int i);
