71d3adab9ab9c8b6d4094bc4bf12a8f5d154c9ee
victor <victormburu03@gmail.com>
2023-08-07 11:47:31 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,10 +23,8 @@ char *str_concat(char *s1, char *s2)
 	while (s2[ci] != '\0')
 		ci++;
 	conct = malloc(sizeof(char) * (i + ci + 1));
-			
 		if (conct == NULL)
 			return (NULL);
-	
 	i = ci = 0;
 	while (s1[i] != '\0')
 	{
