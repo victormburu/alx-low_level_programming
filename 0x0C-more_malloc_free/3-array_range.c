35506dc70f1e52ac82ceae8f52bdccca797e39fd
victor <victormburu03@gmail.com>
2023-08-10 13:48:29 -0700

    101-mul.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,14 +16,13 @@ int *array_range(int min, int max)
 		return (NULL);
 
 	a = malloc((sizeof(int) * (max - min) + sizeof(int)));
-	 if (a == NULL)
-		 return (NULL);
-
-	 while (min <= max)
-	 {
-		 a[b] = min;
-		 b++;
-		 min++;
-	 }
-	 return (a);
+	if (a == NULL)
+		return (NULL);
+	while (min <= max)
+	{
+		a[b] = min;
+		b++;
+		min++;
+	}
+	return (a);
 }
