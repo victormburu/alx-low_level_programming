e8aac8c896faec0dc0710a29459c5c63dd0c86c7
victor mburu <victormburu03@gmail.com>
2023-08-29 12:56:36 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,9 +13,11 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int node;
 
 	if (copy == NULL)
+	{
 		*head = (*head)->next;
-	free(copy);
-	return (1);
+		free(copy);
+		return (1);
+	}
 
 	for (node = 0; node < (index - 1); node++)
 	{
